f801577d51be4ce03e740583ae44ba9d56e7ec50
iamezell <iamezell@gmail.com>
2022-06-17 11:26:10 -0400

    adding in the working extension to build off of

---
+++ .history/popup_20220617111401.js
@@ -0,0 +1,22 @@
+let changeColor = document.getElementById("changeColor");
+
+chrome.storage.sync.get("color", ({color}) => {
+    changeColor.style.backgroundColor = color;
+});
+
+// When the button is clicked, inject setPageBackgoundColor into current page
+changeColor.addEventListener("click", async () => {
+ let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+
+ chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    function: setPageBackgroundColor,
+ });
+
+ // The body of this function will be executed as a content script inside the current page
+ function setPageBackgoundColor(){
+    chrome.storage.sync.sync.get("color",{
+        document.body.style.backgroundColor = color;
+    })
+ }
+});
